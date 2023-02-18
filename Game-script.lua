ad9b4eaa98e2a3e26b98a5dccf87e5d4eefd634a
comet3456998765 <109533854+comet3456998765@users.noreply.github.com>
2023-02-18 09:25:43 -0600

    Update Game-script.lua

--- Game-script.lua
+++ Game-script.lua
@@ -1,5 +1,6 @@
 local games = {
-    [{606849621}] = "https://raw.githubusercontent.com/comet3456998765/jailbreakgui/main/Jailbreakguiscript.lua"
+    [{606849621}] = "https://raw.githubusercontent.com/comet3456998765/jailbreakgui/main/Jailbreakguiscript.lua",
+    [{413424176}] = "https://raw.githubusercontent.com/comet3456998765/Clone-Tycoon-2/main/YesNetGui"
 }
 
 for ids, url in next, games do
