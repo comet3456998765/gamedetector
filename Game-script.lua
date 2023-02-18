b3dc8db739fdbdf71fd18d7566ca99713a2c1fd6
comet3456998765 <109533854+comet3456998765@users.noreply.github.com>
2023-02-17 19:13:39 -0600

    Update and rename Game-script to Game-script.lua

--- Game-script.lua
+++ Game-script.lua
@@ -1,5 +1,5 @@
 local games = {
-    [{606849621}] = "",
+    [{606849621}] = "https://raw.githubusercontent.com/comet3456998765/jailbreakgui/main/Jailbreakguiscript.lua"
 }
 
 for ids, url in next, games do
