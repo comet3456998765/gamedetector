0ab1379a3335652b128f643cd5cf279b343a8dec
comet3456998765 <109533854+comet3456998765@users.noreply.github.com>
2023-02-24 21:04:52 -0600

    Update Game-script.lua

--- Game-script.lua
+++ Game-script.lua
@@ -4,20 +4,20 @@ local base = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
 local texto = Instance.new("TextLabel")
 local UICorner_2 = Instance.new("UICorner")
-
+ 
 -- Properties:
 Intro.Name = "Intro"
 Intro.Parent = game.CoreGui
 Intro.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
+ 
 base.Name = "base"
 base.Parent = Intro
 base.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 base.Position = UDim2.new(0.5, -106, 1, -50) -- Start position: centered above the screen
 base.Size = UDim2.new(0, 212, 0, 40)
-
+ 
 UICorner.Parent = base
-
+ 
 texto.Name = "texto"
 texto.Parent = base
 texto.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
@@ -29,9 +29,9 @@ texto.TextColor3 = Color3.fromRGB(255, 255, 255)
 texto.TextScaled = true
 texto.TextSize = 14
 texto.TextWrapped = true
-
+ 
 UICorner_2.Parent = texto
-
+ 
 -- TweenPosition animation:
 local endPosition = UDim2.new(0.5, -106, 0.5, -20)
 local startSize = UDim2.new(0, 212, 0, 0)
@@ -47,16 +47,17 @@ tween1:Play()
 tween1.Completed:Connect(function()
     tween2:Play()
 end)
-
+ 
 -- Change the label text when the animation is completed:
 tween2.Completed:Connect(function()
     wait(3)
-    texto.Text = "Animation completed!"
+    texto.Text = "Please Wait..."
     wait(3)
-    texto.Text = "Animation in dev!"
+    texto.Text = "Please Wait.."
     wait(3)
-    texto.Text = "Script Loaded!!!"
+    texto.Text = "Please Wait."
     wait(3)
+    texto.Text = "Game checked loading script"
     tween2:Cancel()
     local tween3 = game:GetService("TweenService"):Create(base, TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {
         Position = UDim2.new(0.5, -106, 1, -50)
@@ -66,20 +67,25 @@ tween2.Completed:Connect(function()
         base.Visible = false
     end)
 end)
-
-
-wait(4)
+ 
+ 
+wait(13)
 local games = {
     [{606849621}] = "https://raw.githubusercontent.com/comet3456998765/jailbreakgui/main/Jailbreakguiscript.lua",
     [{413424176}] = "https://raw.githubusercontent.com/comet3456998765/Clone-Tycoon-2/main/YesNetGui",
     [{155615604}] = "https://raw.githubusercontent.com/comet3456998765/All-games-new-/main/ForGamedetector",
+    [{13822889}] = "https://raw.githubusercontent.com/comet3456998765/Scripts/main/LT2SCRIPT.lua",
+    [{6516141723}] = "https://raw.githubusercontent.com/comet3456998765/Games/main/(Doors%20script).lua",
+    [{6447798030}] = "https://raw.githubusercontent.com/comet3456998765/Games/main/(FunkyFryday).lua"
 }
-
+ 
 for ids, url in next, games do
     if table.find(ids, game.PlaceId) then
         loadstring(game:HttpGet(url))()
         break
     end
 end
+
+wait(5)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Supportsystem/main/file.lua"))()
